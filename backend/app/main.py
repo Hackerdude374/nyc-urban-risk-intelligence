18cf9163377fbff8c6cf64159763075d676559c5
Robert Le <robertle982@gmail.com>
2026-04-26 15:55:02 -0400

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI(
     title="NYC Urban Risk Intelligence API",
     description="Backend API for NYC infrastructure risk analytics, geospatial insights, and ML-powered predictions.",
     version="0.1.0",
 )
-
-
+#connect frontend tor backend with cors
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 def root():
     return {
